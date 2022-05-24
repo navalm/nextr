27fa4adb4cb38e6ae482008f801a3f252d4a338b
Naval Monga <navalmng@gmail.com>
2022-05-24 00:42:00 -0500

    Deploy to gh-pages

---
+++ _next/static/N_1ZI8jBLXBpY5XRvttWJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bbfcc7a83b8b9d34.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
